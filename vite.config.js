ae7c7f63039fbfc493e3dfaf76e836e68a9fbb7d
Logan Night <96597232+flyboy-byte@users.noreply.github.com>
2026-05-19 22:12:11 -0500

    build: point Vite input to index.src.html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  export default defineConfig({
-    plugins: [react()],
-    base: "/portfolio/",
-  });
+import react from "@vitejs/plugin-react";
+export default defineConfig({
+  plugins: [react()],
+  base: "/portfolio/",
+  build: {
+    rollupOptions: {
+      input: "index.src.html",
+    },
+  },
+});
