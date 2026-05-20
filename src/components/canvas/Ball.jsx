28acd6521494179e3c13dab08ce7a0a31c7cde87
Logan Night <96597232+flyboy-byte@users.noreply.github.com>
2026-05-19 22:41:58 -0500

    fix: ball spring auto-rotate + damping

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,18 +1,17 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Ball = React.memo(({ iconUrl }) => {
   const [decal] = useTexture([iconUrl]);
-
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
+        <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
         <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} scale={1} map={decal} flatShading />
       </mesh>
     </Float>
@@ -20,10 +19,30 @@ const Ball = React.memo(({ iconUrl }) => {
 });
 
 const BallCanvas = ({ icon }) => {
+  const controlsRef = useRef();
+
+  const handleStart = () => {
+    if (controlsRef.current) controlsRef.current.autoRotate = false;
+  };
+  const handleEnd = () => {
+    if (controlsRef.current) controlsRef.current.autoRotate = true;
+  };
+
   return (
-    <Canvas frameloop="demand" dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas frameloop="always" dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
+        <OrbitControls
+          ref={controlsRef}
+          enableZoom={false}
+          autoRotate
+          autoRotateSpeed={3}
+          enableDamping
+          dampingFactor={0.08}
+          maxPolarAngle={Math.PI * 0.75}
+          minPolarAngle={Math.PI * 0.25}
+          onStart={handleStart}
+          onEnd={handleEnd}
+        />
         <Ball iconUrl={icon} />
       </Suspense>
       <Preload all />
